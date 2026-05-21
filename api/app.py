462c43999d4ddbbca41c6b8df28585b981789e73
yvesdylane <yvesdylane24@gmail.com>
2026-05-21 16:11:01 +0100

    set up uni pile to strat receiving messages

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from tools.router import ToolRouter
 
@@ -7,6 +7,18 @@ router = ToolRouter()
 
 class MessageRequest(BaseModel):
     message: str
+
+@app.get("/")
+def home():
+    return {"message": "Server working. Wellcome to moonsu"}
+
+@app.post("/whatsapp")
+async def webhook(request: Request):
+    data = await request.json()
+    if data["event"] == "message_received" :
+        print(data["account_info"]["phone_number"])
+
+    return {"status": "received"}
 
 @app.post("/chat")
 def chat(request: MessageRequest):
